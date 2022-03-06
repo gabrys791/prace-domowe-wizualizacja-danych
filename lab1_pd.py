abc11a1435066501f09b63962a42124b0bc78e55
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-03-06 17:18:55 +0100

    Add files via upload

---
+++ lab1_pd.py
@@ -0,0 +1,93 @@
+#zad1
+# a=5
+# d=4
+# b='Jestem Gabriel'
+# e='ucze sie pytona'
+# c=2.5
+# f=6.25
+# g=2+3j
+# h=1j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+
+
+
+#zad2
+# liczba1=input("podaj liczbe 1: ")
+# liczba1=int(liczba1)
+# liczba2=input("podaj liczbe 2: ")
+# liczba2=int(liczba2)
+# znak=input("podaj co chcesz zrobić z dwiema liczbami: ")
+# if znak=='+':
+#     print(liczba1+liczba2)
+# elif znak=='-':
+#     print(liczba1-liczba2)
+# elif znak=='/':
+#     print(liczba1/liczba2)
+# elif znak=='*':
+#     print(liczba1*liczba2)
+
+
+
+#zad3
+# a=1
+# a+=1
+# print(a)
+# b=2
+# b-=1
+# print(b)
+# c=3
+# c*=1
+# print(c)
+# d=4
+# d/=2
+# print(d)
+# e=5
+# e**=2
+# print(e)
+# f=6
+# f%=2
+# print(f)
+#zad 4
+# import math
+# e=2.72
+# print(e**10)
+# potega=pow(math.log(5+math.sin(8)**2),1/6)
+# print(potega)
+# a=3.55
+# print(int(a))
+# b=4.80
+# print(int(b)+1)
+#zad 5
+# imie="GABRIEL"
+# nazwisko="MAJEWSKI"
+# print(imie.capitalize(),nazwisko.capitalize())
+#zad 6
+# tekst="hej la la la cos cos cos la la la nawet nie wiem co ja pisze"
+# print(tekst.count('la'))
+#zad 7
+# imie="Gabriel"
+# print(imie[1])
+# print(imie[6])
+#zad 8
+# tekst="hej la la la cos cos cos la la la nawet nie wiem co ja pisze"
+# print(tekst.split())
+#zad 9
+# imie="Majewski"
+# a=2.5
+# b=16
+# hex(b)
+# print('%(z1)d %(z2)s %(z3)d' %{'z1':a, 'z2':imie ,'z3':b})
+
+
+
+
+
+
+
