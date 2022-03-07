a40f8ba46cc4b139fc88fefbe5605685cdba95a8
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-03-07 17:34:14 +0100

    Add files via upload

---
+++ pd_lab2.py
@@ -0,0 +1,63 @@
+#zad 1
+lista_sportów=['siatkówka','koszykówka','tenis-stołowy']
+lista_sportów.reverse()
+print(lista_sportów)
+lista_sportów.append('piłka nożna')
+lista_sportów.append('boks')
+print(lista_sportów)
+#zad 2
+# słownik={'cdn':'ciąg dalszy nastąpi','CV':'curriculum vitae','lic':'licencjat','mgr':'magister','dr':'doktor'}
+# print(słownik)
+#zad 3
+# słownik={1:'League of legends',2:'Valorant',3:'Kena Bridge of Spirits',4:'Apex legends'}
+# print(len(słownik))
+#zad 4
+# tekst=input('napisz tekst: ')
+# print(tekst.count('a'))
+#zad 5
+# import sys as system
+# system.stdout.write('wczytaj a')
+# a=system.stdin.readline()
+# a=int(a)
+# system.stdout.write('wczytaj b')
+# b=system.stdin.readline()
+# b=int(b)
+# system.stdout.write('wczytaj c')
+# c=system.stdin.readline()
+# c=int(c)
+# print(a**b+c)
+#zad 6
+# a=input("podaj liczbe a: ")
+# b=input("podaj liczbe b: ")
+# c=input("podaj liczbe c: ")
+# if (a>b) & (a>c):
+#     print("a jest największe")
+# if (b>a) & (b>c):
+#     print("b jest największe")
+# if (c>a) & (c>b):
+#     print("c jest największe")
+#zad 7
+# lista=[1,2,3,4,5,5.5]
+# for i in lista:
+#     print(i**2)
+#zad 8
+# z=0
+# lista=[]
+# while z!=10:
+#    if z%2==0:
+#      lista.append(z)
+#    z+=1
+# print(lista)
+#zad 9
+# a=input("z czego mam wyciągnąć pierwiastek?")
+# a=int(a)
+# if a<0:
+#    print("tak nie wolno")
+# else:
+#    print(pow(a,1/2))
+
+
+
+
+
+
