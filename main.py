7f4a59827ba17a02ccc33a395e82637dacd1e7a5
gabrys791 <100271821+gabrys791@users.noreply.github.com>
2022-04-13 10:26:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import numpy as np
+#zad1
+# a=np.arange(3)
+# print(a)
+# b=np.arange(3)
+# print(b)
+# print(a*b)
+#zad2
+# a=np.arange(9).reshape(3,3)
+# b=np.arange(16).reshape(4,4)
+# print(a)
+# print(b)
+# print(a.min(axis=0))
+# print(a.min(axis=1))
+# print(b.min(axis=0))
+# print(b.min(axis=1))
+#zad3
+# a=np.arange(3)
+# print(a)
+# b=np.arange(3)
+# print(b)
+# print(a.dot(b))
+#zad4
+# a=np.array([2,3,4])
+# b=np.array([2.5,2.3,1.2])
+# print(a*b)
+#zad5
+# b=np.arange(0,12,2).reshape(2,3)
+# print(b)
+# a=np.sin(b)
+# print(a)
+# #zad6
+# c=np.arange(0,18,3).reshape(2,3)
+# print(c)
+# b=np.cos(c)
+# print(b)
+
+#zad7
+# print(a+b)
+#zad8
+# a=np.arange(9).reshape(3,3)
+# for i in a:
+#     print(i)
+#zad9
+# a=np.arange(9).reshape(3,3)
+# print(a)
+# for i in a.flat:
+#     print(i)
+#zad10
+# a=np.arange(0,81).reshape(9,9)
+# print(a)
+# b=a.reshape(27,3)
+# print(b)
+#zad11
+a=np.arange(12)
+print(a)
+b=a.reshape(3,4)
+print(b)
+c=a.reshape(4,3)
+print(c)
+d=a.reshape(2,6)
+print(d)
